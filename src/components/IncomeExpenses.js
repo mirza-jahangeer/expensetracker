2f857ef450ce234367ffc2bc31cd732b8bd724b5
Mirza Jahangeer <41241041+mirza-jahangeer@users.noreply.github.com>
2020-08-02 01:29:03 +0500

    Update IncomeExpenses.js

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -8,15 +8,9 @@ export const IncomeExpenses = () => {
     //     return accumulator+=currentValue;
     // }, 0).toFixed(2);
 
-    const income = amount.
-    filter(item => item>0).
-    reduce((accumulator, currenValue) => (accumulator+=currenValue), 0)
-    .toFixed(2);
+    const income = amount.filter(item => item>0).reduce((accumulator, currenValue) => (accumulator+=currenValue), 0).toFixed(2);
 
-    const expense = (amount
-    .filter(item => item<0)
-    .reduce((accumulator, currentValue) => (accumulator+=currentValue), 0) * -1)
-    .toFixed(2);
+    const expense = (amount.filter(item => item<0).reduce((accumulator, currentValue) => (accumulator+=currentValue), 0) * -1).toFixed(2);
 
     return (
         <div className="inc-exp-container">
